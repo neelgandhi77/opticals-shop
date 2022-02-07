86f73c8feadb9d68a72da53739d9b5fcb93f4727
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2022-02-07 12:36:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, g, render_template, request
 import pyrebase
+import datetime
 
+from sqlalchemy import false, null
 app = Flask(__name__)
 
 
@@ -15,36 +17,56 @@ config = {
     "measurementId": "G-V8BYKQZ0LK"
 }
 
-
+flag = 0
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-idlist = ["Not Exist"]
+#idlist = ["Not Exist"]
 searchlist = []
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    global flag
+    if (flag):
+        fname = request.form['fname'].lower()
+        mname = request.form['mname'].lower()
+        lname = request.form['lname'].lower()
+        mb = request.form['mb']
+        id = flag[0:10]
+        child1 = flag[10:]
+        if(id != "" and len(mb) != 0):
+            db.child(id).child(child1).update(
+                {'First  Name': fname, 'Middle Name': mname, 'Last   Name': lname, 'Mobile     ': str(mb)})
+        flag = 0
+        return render_template('index.html')
     try:
-        if request.method == 'POST':
+
+        if request.method == 'POST' and flag == 0:
             fname = request.form['fname'].lower()
             mname = request.form['mname'].lower()
             lname = request.form['lname'].lower()
             mb = request.form['mb']
-            title = (fname[0]+mname[0]+lname[0] +
-                     "-" + mb[-3] + mb[-2]+mb[-1]).upper()
-            idlist.append(title)
+            # title = (fname[0]+mname[0]+lname[0] +
+            # +8"-" + mb[-3] + mb[-2]+mb[-1]).upper()
+            current_time = datetime.datetime.now()
+            title = mb
+            child1 = str(current_time.day) + "-" + str(current_time.month) + "-" + str(current_time.year) + \
+                " " + str(current_time.hour) + ":" + \
+                str(current_time.minute) + ":" + str(current_time.second)
+            # idlist.append(title)
+
             if(title != "" and len(mb) != 0):
+                db.child(title).child(child1).set(
+                    {'First  Name': fname, 'Middle Name': mname, 'Last   Name': lname, 'Mobile     ': str(mb), 'ydata     ': str(mb)+child1})
 
-                db.child(title).child("").set(
-                    {'First  Name': fname, 'Middle Name': mname, 'Last   Name': lname, 'Mobile     ': mb})
+            return render_template('index.html')
 
-            return render_template('index.html', id_display=idlist[-1])
-        return render_template('index.html', id_display=idlist[-1])
+        return render_template('index.html')
 
     except:
 
-        return render_template('index.html')
+        return render_template('alert.html')
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -55,23 +77,30 @@ def search():
             id = request.form['id'].upper()
             searchlist.append(id)
             data = db.child(id).child("").get().val().values()
+            data = list(data)
             return render_template('search.html', data=data)
         return render_template('search.html', data=data)
     except:
         return render_template('search.html')
 
 
-@app.route('/update', methods=['GET', 'POST'])
-def update():
+@app.route('/update/<string:ydata>', methods=['GET', 'POST'])
+def update(ydata):
     try:
-        id = searchlist[-1]
-        data = db.child(id).child("").get().val().values()
+        #id = searchlist[-1]
+        id = ydata[0:10]
+        child1 = ydata[10:]
+        print(id, child1)
+        #data = db.child(id).child("").get().val().values()
+        data = db.child(id).child(child1).get().val().values()
         data = list(data)
         temp = []
         for i in data:
             temp.append(i)
-        db.child(id).remove()
-        return render_template('update.html', fname=temp[0], mname=temp[1], lname=temp[2], mb=temp[3])
+        global flag
+        flag = ydata
+        # db.child(id).remove()
+        return render_template('update.html', fname=temp[0], lname=temp[1], mname=temp[2], mb=temp[3])
     except:
         return render_template('alert.html')
 
